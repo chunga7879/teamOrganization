007f5bc8f7cab6e3fab6aaccaad154ca0d2edc78
chunga7879 <chunga7879@gmail.com>
2022-02-05 11:12:10 -0800

    sdf

--- src/main/model/SkillSet.java
+++ src/main/model/SkillSet.java
@@ -4,5 +4,21 @@ import java.util.List;
 
 public class SkillSet {
 
+	boolean java;
+	boolean kotlin;
+	boolean cplusplus;
+	boolean c;
+	boolean chash;
+	boolean python;
+	boolean js;
+	boolean embeddedSoftware;
+	boolean linux;
+	boolean html;
+	boolean css;
+	boolean xml;
+	boolean groovy;
+	boolean redux;
+	boolean vueJS;
+	boolean jQuery;
 
 }
