cb46163c508fe4b4f0e5103405f6b29414212381
CassielJung <eunjijung1101@gmail.com>
2022-02-05 11:26:04 -0800

    add skills sub-classes

---
+++ src/main/model/skills/SoftwareEngineering.java
@@ -0,0 +1,18 @@
+public class SoftwareEngineering {
+    boolean java;
+	boolean kotlin;
+	boolean cplusplus;
+	boolean c;
+	boolean chash;
+	boolean python;
+	boolean js;
+	boolean embeddedSoftware;
+	boolean linux;
+	boolean html;
+	boolean css;
+	boolean xml;
+	boolean groovy;
+	boolean redux;
+	boolean vueJS;
+	boolean jQuery;
+}
