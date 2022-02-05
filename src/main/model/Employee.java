814079358e64631348151248f238125d00acfc04
chunga7879 <chunga7879@gmail.com>
2022-02-05 10:51:03 -0800

    one

---
+++ src/main/model/Employee.java
@@ -0,0 +1,13 @@
+package main.model;
+
+public class Employee {
+    String employeeName;
+    String email;
+    SkillSet skillSets;
+
+
+    public Employee() {
+
+    }
+
+}
---
+++ src/main/model/SkillSet.java
@@ -0,0 +1,8 @@
+package main.model;
+
+import java.util.List;
+
+public class SkillSet {
+
+
+}
